71cbb42ea84b11a9d1f920cf2400d2f8434366c3
Garrett Pierce <gfpierce2@gmail.com>
2018-10-18 23:14:30 -0400

    Started CSS and HTML
    - Coded out base HTML
    - Coded out some CSS through bill and split input boxes
    - Outlined JS functionality

---
+++ app.js
@@ -0,0 +1,13 @@
+/*TODO
+Calculate Splits
+    1. Enter bill (this updates bill value in the top)
+    2. Take in number of splits
+    3. Calculate individual splits
+    4. Display individual splits
+
+Tip on each split
+    1. Add tip percentage entry (HTML input) for each split
+    2. Calculate and display each tip when entered
+    3. Calculate and display total
+
+*/
